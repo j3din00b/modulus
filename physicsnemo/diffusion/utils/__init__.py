7b26bbb2cd00c49d81e6cc2e4018a68a2286f739
Peter Harrington <48932392+pzharrington@users.noreply.github.com>
2026-02-04 19:18:56 -0800

    Add concatenation wrapper for legacy diffusion models (#1366)
    * Add concatenation wrapper for legacy diffusion models
    
    * docstring fix
    
    * safer torch import
    
    * Address feedback, polish docstrings
    
    * Add warning for tensor arg
    
    * lint
    
    * Update dit defaults for doctest
    
    * Actually update defaults
    
    * license headers

--- physicsnemo/diffusion/utils/__init__.py
+++ physicsnemo/diffusion/utils/__init__.py
@@ -14,4 +14,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .model_wrappers import ConcatConditionWrapper
 from .utils import InfiniteSampler, StackedRandomGenerator
